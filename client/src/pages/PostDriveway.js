bab190c5bdc1e3511e1dede0586988130a010ee0
Wonjong Park <wonjong2@gmail.com>
2022-08-05 16:27:46 -0700

    Updating UI for PostDriveway

--- client/src/pages/PostDriveway.js
+++ client/src/pages/PostDriveway.js
@@ -51,97 +51,137 @@ function PostDriveway() {
         <div className="container my-1">
             <h2>Post Driveway</h2>
             <form onSubmit={handleFormSubmit}>
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="address">Address:</label>
+                <div className="input-group mx-auto">
+                    <span className="input-group-text" id="basic-addon1">Address</span>
                     <input
+                        type="text"
+                        class="form-control"
                         placeholder="Address"
+                        aria-describedby="basic-addon1"
                         name="address"
-                        type="text"
                         id="address"
-                        onChange={handleChange}
-                    />
+                        onChange={handleChange} />
                 </div>
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="description">Description:</label>
+                <div className="input-group mx-auto">
+                    <span className="input-group-text" id="basic-addon1">Description</span>
                     <input
+                        type="text"
+                        class="form-control"
                         placeholder="Description"
+                        aria-describedby="basic-addon1"
                         name="description"
-                        type="text"
                         id="description"
-                        onChange={handleChange}
-                    />
+                        onChange={handleChange} />
                 </div>
-                <div className="flex-row space-between my-2">
+                {/* <div className="flex-column space-between my-2">
+                    <label htmlFor="address">Address:</label>
+                    <div className="input-group">
+                        <input
+                            placeholder="Address"
+                            name="address"
+                            type="text"
+                            id="address"
+                            onChange={handleChange}
+                        />
+                    </div>
+                </div>
+                <div className="flex-column space-between my-2">
+                    <label htmlFor="description">Description:</label>
+                    <div className="input-group">
+                        <input
+                            placeholder="Description"
+                            name="description"
+                            type="text"
+                            id="description"
+                            onChange={handleChange}
+                        />
+                    </div>
+                </div>
+                <div className="flex-column space-between my-2">
                     <label htmlFor="rules">Rules:</label>
-                    <textarea
-                        placeholder="Rules"
-                        name="rules"
-                        type="text"
-                        id="rules"
-                        onChange={handleChange}
-                    />
+                    <div className="input-group">
+                        <textarea
+                            placeholder="Rules"
+                            name="rules"
+                            type="text"
+                            id="rules"
+                            onChange={handleChange}
+                        />
+                    </div>
                 </div>
-                <div className="flex-row space-between my-2">
+                <div className="flex-column space-between my-2">
                     <label htmlFor="price">Price($ per hour):</label>
-                    <input
-                        placeholder="$$"
-                        name="price"
-                        type="number"
-                        id="price"
-                        onChange={handleChange}
-                    />
+                    <div className="input-group">
+                        <input
+                            placeholder="$$"
+                            name="price"
+                            type="number"
+                            id="price"
+                            onChange={handleChange}
+                        />
+                    </div>
                 </div>
-                <div className="flex-row space-between my-2">
+                <div className="flex-column space-between my-2">
                     <label htmlFor="date">Available Date:</label>
-                    <input
-                        placeholder="MM/DD/YYYY"
-                        name="availableDate"
-                        type="date"
-                        id="date"
-                        onChange={handleChange}
-                    />
+                    <div className="input-group">
+                        <input
+                            placeholder="MM/DD/YYYY"
+                            name="availableDate"
+                            type="date"
+                            id="date"
+                            onChange={handleChange}
+                        />
+                    </div>
                 </div>
-                <div className="flex-row space-between my-2">
+                <div className="flex-column space-between my-2">
                     <label htmlFor="start">Start Time:</label>
-                    <input
-                        placeholder="HH"
-                        name="startTime"
-                        type="time"
-                        id="start"
-                        onChange={handleChange}
-                    />
+                    <div className="input-group">
+                        <input
+                            placeholder="HH"
+                            name="startTime"
+                            type="time"
+                            id="start"
+                            onChange={handleChange}
+                        />
+                    </div>
                 </div>
-                <div className="flex-row space-between my-2">
+                <div className="flex-column space-between my-2">
                     <label htmlFor="end">End Time:</label>
-                    <input
-                        placeholder="HH"
-                        name="endTime"
-                        type="time"
-                        id="end"
-                        onChange={handleChange}
-                    />
+                    <div className="input-group">
+                        <input
+                            placeholder="HH"
+                            name="endTime"
+                            type="time"
+                            id="end"
+                            onChange={handleChange}
+                        />
+                    </div>
                 </div>
-                <div className="flex-row space-between my-2">
+                <div className="flex-column space-between my-2">
                     <label htmlFor="zip">ZIP Code:</label>
-                    <input
-                        placeholder="ZIP"
-                        name="zipcode"
-                        type="number"
-                        id="zip"
-                        onChange={handleChange}
-                    />
+                    <div className="input-group">
+                        <input
+                            placeholder="ZIP"
+                            name="zipcode"
+                            type="number"
+                            id="zip"
+                            onChange={handleChange}
+                        />
+                    </div>
                 </div>
-                <div className="flex-row space-between my-2">
+                <div className="flex-column space-between my-2">
                     <label htmlFor="image">Photo:</label>
-                    <input
-                        placeholder="filename"
-                        name="image"
-                        type="text"
-                        id="image"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="flex-row flex-end">
+                    <div className="input-group">
+                        <input
+                            placeholder="filename"
+                            name="image"
+                            type="text"
+                            id="image"
+                            onChange={handleChange}
+                        />
+                    </div>
+                </div> */}
+                <div className="flex-column flex-end">
                     <button type="submit">Submit</button>
                 </div>
             </form>
