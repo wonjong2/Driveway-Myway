9c1c1519744892e73ff0880a787a101f02f3d9e6
Ayami Higashi <ayamibhigashi@gmail.com>
2022-08-06 10:37:19 -0700

    comment out cart function on success page to redirect to homepage

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -7,27 +7,27 @@ import { idbPromise } from '../utils/helpers';
 function Success() {
   const [addOrder] = useMutation(ADD_ORDER);
 
-  useEffect(() => {
-    async function saveOrder() {
-      const cart = await idbPromise('cart', 'get');
-      const products = cart.map((item) => item._id);
+  // useEffect(() => {
+  //   async function saveOrder() {
+  //     const cart = await idbPromise('cart', 'get');
+  //     const products = cart.map((item) => item._id);
 
-      if (products.length) {
-        const { data } = await addOrder({ variables: { products } });
-        const productData = data.addOrder.products;
+  //     if (products.length) {
+  //       const { data } = await addOrder({ variables: { products } });
+  //       const productData = data.addOrder.products;
 
-        productData.forEach((item) => {
-          idbPromise('cart', 'delete', item);
-        });
-      }
+  //       productData.forEach((item) => {
+  //         idbPromise('cart', 'delete', item);
+  //       });
+  //     }
 
       setTimeout(() => {
         window.location.assign('/');
       }, 3000);
-    }
+  //   }
 
-    saveOrder();
-  }, [addOrder]);
+  //   saveOrder();
+  // }, [addOrder]);
 
   return (
     <div>
