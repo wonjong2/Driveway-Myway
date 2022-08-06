5e20ca700644907caded036a83a388e00f7437c6
Wonjong Park <wonjong2@gmail.com>
2022-08-06 15:47:11 -0700

    Update date handling code

---
+++ server/utils/date.js
@@ -0,0 +1,10 @@
+// Load 'day.js' package 
+const dayjs = require('dayjs');
+
+// Getter for createdAt property in thoughtSchema and reactionSchema
+function timeStamp(date) {
+    // e.g.) Aug 16, 2018 8:02 PM
+    return dayjs(date).format('MMMM D, YYYY');
+}
+
+module.exports = { timeStamp };
